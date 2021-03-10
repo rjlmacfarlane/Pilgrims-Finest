ffe769745e03350886d859bcc2f3af4334852bf3
Mathius <devachievem@gmail.com>
2021-03-09 22:32:49 -0800

    evenly space and center products in products.js

--- src/Components/ProductListItem.jsx
+++ src/Components/ProductListItem.jsx
@@ -8,19 +8,19 @@ const ProductListItem = ({ product }) => {
 
   return (
     <div className="item-wrap">
-      <div className="w-96 h-96 flex relative group">
+      <div className="">
       <img src={bgImage} alt=""></img>
 
         <div
-          className="absolute inset-0 bg-cover bg-center z-0"
+          className=""
           style={bgImage}
         ></div>
-        <div className="absolute inset-0 bg-cover bg-center z-0 h-84 group-hover:bg-black group-hover:opacity-50"></div>
-        <div className="opacity-0 group-hover:opacity-100 duration-300 absolute top-12 px-6 text-cornsilk font-semibold flex flex-col justify-center items-center">
-          <div className="z-10 flex justify-center items-center text-center text-3xl">
+        <div className=""></div>
+        <div className="">
+          <div className="">
             {product.name}
           </div>
-          <div className="z-10 flex justify-center items-center">
+          <div className="">
             <img src={bgImage} alt=""></img>
             {product.description}
           </div>
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -6,17 +6,16 @@ class Products extends Component {
     if(this.props.data){
       var products = this.props.data.products.map(function(products){
         var projectImage = 'images/products/'+products.photo;
-        return <div key={products.name} className="columns portfolio-item">
+        return <div key={products.name} className="columns portfolio-item product-item">
            <div className="item-wrap">
-            <a href="" title={products.name}>
-               <img alt={products.name} src={projectImage} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{products.name}</h5>
-                     <p>{products.description}</p>
-                  </div>
+            <a href="/" title={products.name}>
+              <img alt={products.name} src={projectImage} />
+              <div className="overlay">
+                <div className="portfolio-item-meta">
+                <h5>{products.name}</h5>
+                    <p>{products.description}</p>
                 </div>
-              
+              </div>
             </a>
           </div>
         </div>
@@ -25,10 +24,10 @@ class Products extends Component {
 
     return (
       <section id="products">
-        <div className="row">
+        <div className="">
           <div className="ten columns collapsed">
             <h1>Products</h1>
-              <div id="products-wrapper" className="bgrid-halves s-bgrid-quarters cf">
+              <div id="products-wrapper" className="">
                   {products}
               </div>
           </div>
