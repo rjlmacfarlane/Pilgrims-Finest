cd10ae4526eba8a92aafef0944289dbaf62015a2
rjlmacfarlane <rjl.macfarlane@gmail.com>
2021-03-10 12:58:13 -0400

    Stylistic updates

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,8 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
+              <li>Pilgrim's Finest and associated content &copy; 2021 Pilgrim's Finest</li>
+              <li>Developed by <a title="Ryan MacFarlane" href="https://github.com/rjlmacfarlane/">Ryan MacFarlane</a> and <a title="Mathius Johnson" href="https://github.com/mathiusjohnson">Mathius Johnson</a></li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
